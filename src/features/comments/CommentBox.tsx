db5a92d969f2502bceed95ceb5a4024dfa838445
Fabio <fc_102@hotmail.com>
2023-03-21 23:30:50 -0300

    allow post to the comment collection

--- src/features/comments/CommentBox.tsx
+++ src/features/comments/CommentBox.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '../../config/config'
 
 const CommentBox = () => {
   const [comment, setComment] = useState<string>('')
-  console.log(comment)  
 
-  const submitComment = () => {
-
+  const commentRef = collection(db, 'comentário')  
+ 
+  const submitComment = async(event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    await addDoc(commentRef, {
+      comment: comment,
+      //TODO ADD ID GENERATOR FOR TS
+      id: '55',
+      username: 'eu'
+    })
+    console.log('submited')
   }  
 
   return (
-    <form className="max-w-[1240px] mx-auto font-serif py-5">
-        <textarea value={comment} onChange={(event) => setComment(event.target.value)} className='w-full' placeholder='Faça um comentário...'/>
+    <form onSubmit={submitComment} className="max-w-[1240px] mx-auto font-serif py-5">
+        <textarea value={comment} onChange={(event) => setComment(event.target.value)} className='w-full' placeholder='Faça um comentário...' />
         <br />
         <button type="submit">Submit</button>
-    </form>
+  </form>
   )
 }
 
